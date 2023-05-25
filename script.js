5c3ba378356859faeb5a9f81d22550d1a4b198f3
Rheiana Cuevas <rmcuevas@ucdavis.edu>
2023-05-25 14:51:46 -0700

    Co-authored-by: Laura Yien <lyien@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import {loadGLTF, loadTexture} from "/libs/loader.js";
+const THREE = window.MINDAR.FACE.THREE;
+
 (function(){
 
     //Variables
@@ -5,10 +8,13 @@
     //Pages
     const homePage = document.querySelector('#homepage');
     const mapPage = document.querySelector('#mappage');
+    const popup = document.querySelectorAll(".popup")
 
     //Buttons
     const startBtn = document.querySelector('#homepage .primary');
     const instructions = document.querySelector('#homepage .secondary');
+    const camera = document.querySelector('#camera');
+    const faceFilterSection = document.querySelector('#facefilter');
 
     // Show/hide functions
     startBtn.addEventListener('click', function(){
@@ -17,9 +23,65 @@
     })
 
     // Davis Leaflet Map
-    var map = L.map('map').setView([38.544087, -121.743363],17);
+    var map = L.map('map').setView([38.544087, -121.743363],13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);  
+
+    var centralpark = L.marker([38.5453, -121.7445]).addTo(map);
+
+    centralpark.addEventListener("touchstart", function (){
+        popup.classList.replace("hidden", "showing")
+    });
+
+    var bikemuseum = L.marker([38.5443, -121.7443]).addTo(map);
+
+    var arb = L.marker([38.5402, -121.7417]).addTo(map);
+
+    var seal = L.marker([38.5432, -121.7406]).addTo(map);
+
+    var egghead = L.marker([38.5419, -121.7478]).addTo(map);
+
+    var train = L.marker([38.5436, -121.7376]).addTo(map);
+
+
+
+    // Face Filter
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const start = async() => {
+            const mindarThree = new window.MINDAR.FACE.MindARThree({
+            container: faceFilterSection,
+          });
+          const {renderer, scene, camera} = mindarThree;
+      
+          const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+          scene.add(light);
+      
+          //facemesh variable ; different from anchor; returned face mesh 
+          const faceMesh = mindarThree.addFaceMesh();
+          // const texture = await loadTexture("../../assets/facemesh/face-mask-template/Face_Mask_Template.png");
+          const texture = await loadTexture("/assets/test.PNG");
+      
+          faceMesh.material.map = texture;
+          faceMesh.material.transparent = true;
+          faceMesh.material.needsUpdate = true;
+      
+          scene.add(faceMesh);
+      
+          await mindarThree.start();
+          renderer.setAnimationLoop(() => {
+            renderer.render(scene, camera);
+          });
+        }
+        camera.addEventListener('click', () => {
+            faceFilterSection.className = 'showing';
+            homePage.className = 'hidden';
+            start();
+          });
+    });
+    
+      
+
 })();
