3a84be18198e1f1aacb1f743b249a1169eee4cf2
Rheiana Cuevas <rmcuevas@ucdavis.edu>
2023-05-25 01:39:00 -0700

    implemented show/hide -figure out back btn, bg img

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 (function(){
 
+    //Variables
+
+    //Pages
+    const homePage = document.querySelector('#homepage');
+    const mapPage = document.querySelector('#mappage');
+
+    //Buttons
+    const startBtn = document.querySelector('#homepage .primary');
+    const instructions = document.querySelector('#homepage .secondary');
+
+    // Show/hide functions
+    startBtn.addEventListener('click', function(){
+        homePage.className = 'hidden';
+        mapPage.className = 'showing';
+    })
+
     // Davis Leaflet Map
     var map = L.map('map').setView([38.544087, -121.743363],17);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
